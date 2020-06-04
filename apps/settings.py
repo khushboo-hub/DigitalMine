c7bf3fb0f8a979749d68eebca085bf8ec9457c6a
Dewangshu <devwangshu@gmail.com>
2020-06-04 15:47:52 +0530

    renamed find.py and install.py

--- apps/settings.py
+++ apps/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cimfr_iot',
+        'NAME': 'barun_iot',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
