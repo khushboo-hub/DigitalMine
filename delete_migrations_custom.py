a02b4d16163509ed8dc07f608107cc1ac6be9461
Vikash Kisku <vikashkisku@gmail.com>
2020-06-05 11:41:20 +0530

    Corrected delete_migrations_custom

--- delete_migrations_custom.py
+++ delete_migrations_custom.py
@@ -1,89 +1,74 @@
 import fnmatch
 import os
 import sys
+class Clean:
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
+    file_exceptions = ['__init__.py']
+    module_exceptions=['.idea','Include','Lib','media','Scripts','static','staticfiles','venv']
+     
 
-exceptions = []
-if len(sys.argv) > 1:
-    for e in sys.argv:
-        exceptions.append(e)
-    exceptions.pop(0)
-else:
-    exceptions = ['__init__.py']
-
-print(exceptions)
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
-# print(ROOT_DIR+"\\accounts")
-exceptions = ['__init__.py']
-for folders in os.listdir(ROOT_DIR):
-    # print(ROOT_DIR+"\\"+folders)
-    if os.path.isdir(ROOT_DIR + "\\" + folders):
-        for files in os.listdir(ROOT_DIR + "\\" + folders):
-            if fnmatch.fnmatch(files, 'migrations'):
-                for migrations in os.listdir(ROOT_DIR + "\\" + folders + "\\" + files):
-                    if fnmatch.fnmatch(migrations, '*'):
-                        if not migrations.endswith(tuple(exceptions)):
-                            print(ROOT_DIR + "\\" + folders + "\\" + files + "\\" + migrations)
+    def __init__(self, exception=None):
+        if exception is not None:
+            for e in exception:
+                self.file_exceptions.append(e)
+            print('self',self.file_exceptions)    
+        
+        self.delete()
+    def delete(self):
+        for folders in os.listdir(self.ROOT_DIR):
+            appFolder = self.ROOT_DIR + "\\" + folders
+            if os.path.isdir(self.ROOT_DIR + "\\" + folders) and not folders.endswith(tuple(self.module_exceptions)):
+                migrationFolder = self.ROOT_DIR + "\\" + folders+ "\\"+ 'migrations'
+                if os.path.isdir(migrationFolder):
+                    isFile = os.path.isfile(os.path.join(migrationFolder,self.file_exceptions[0]))
+                    if not isFile:
+                        with open(os.path.join(migrationFolder, self.file_exceptions[0]), 'w') as fp:
+                            print('__init__.py file created in',migrationFolder)
+                            pass       
+                    for files in os.listdir(migrationFolder):
+                        if not files.endswith(tuple(self.file_exceptions)):
                             try:
-                                os.remove(ROOT_DIR + "\\" + folders + "\\" + files + "\\" + migrations)
+                                print(migrationFolder+"\\"+files+ "removed!")
+                                os.remove(migrationFolder+"\\"+files)
                             except:
                                 pass
+                else:
+                    print('Migration folder created in',appFolder)
+                    os.mkdir(migrationFolder)
+                    with open(os.path.join(migrationFolder, self.file_exceptions[0]), 'w') as fp: 
+                        print('__init__.py file created in',migrationFolder)
+                        pass
 
+if __name__ == "__main__":
+    arguments = []
+    exceptions=[]
+    fileName=sys.argv[0]
+    try:
+        if len(sys.argv) > 1:
+            for e in sys.argv:
+                arguments.append(e)
+            arguments.pop(0)
+            if arguments[0] == '--clean':
+                if arguments[1] == '--exceptions':
+                    for a in arguments:
+                        exceptions.append(a)
+                    exceptions.pop(0)
+                    exceptions.pop(0)
+                    Clean(exceptions)
+                elif arguments[1] == '--all':
+                    Clean()
 
-# class Clean:
-#     exceptions = ['__init__.py']
-#     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
-
-#     def __init__(self, exception=None):
-#         if exception is not None:
-#             for e in exception:
-#                 self.exceptions.append(e)
-
-#     def delete(self, exceptions):
-#         for folders in os.listdir(self.ROOT_DIR):
-#             if os.path.isdir(self.ROOT_DIR + "\\" + folders):
-#                 for files in os.listdir(self.ROOT_DIR + "\\" + folders):
-#                     if fnmatch.fnmatch(files, 'migrations'):
-#                         for migrations in os.listdir(self.ROOT_DIR + "\\" + folders + "\\" + files):
-#                             if fnmatch.fnmatch(migrations, '*'):
-#                                 if not migrations.endswith(tuple(exceptions)):
-
-#                                     try:
-#                                         os.remove(self.ROOT_DIR + "\\" + folders + "\\" + files + "\\" + migrations)
-#                                         print(
-#                                             self.ROOT_DIR + "\\" + folders + "\\" + files + "\\" + migrations + " Removed Successfully")
-#                                     except:
-#                                         print(
-#                                             self.ROOT_DIR + "\\" + folders + "\\" + files + "\\" + migrations + " Removed Failed")
-#                                         pass
-
-# if __name__ == "__main__":
-#     arguments = []
-#     exceptions=[]
-#     if len(sys.argv) > 1:
-#         for e in sys.argv:
-#             arguments.append(e)
-#     arguments.pop(0)
-
-#     if arguments[0] == '--clean':
-#         print('success')
-#         if arguments[1] == '--exceptions':
-#             print('exceptions')
-#             for a in arguments:
-#                 exceptions.append(a)
-#             exceptions.pop(0)
-#             exceptions.pop(0)
-#             print(exceptions)
-#         elif arguments[1] == '-all':
-#             print('all')
-
-#     elif arguments[0] == 'help':
-#         print('[clean]')
-#         print(" find.py --clean --exceptions",'    for deleting all the files inside migrations. give exceptions if you dont want to delete some files')
-#         print(' find.py--clean --all','            for deleting all the files')
-#     else:
-#         print('Unknown command: ', arguments[0])
-#         print('Type find.py help for usage.')
-
-
-
-
+            elif arguments[0] == 'help':
+                print('[clean]--------for deleting files under migrations')
+                print(fileName,'--clean --exceptions\tgive exceptions if you dont want to delete some files')
+                print(fileName,'--clean --all\t\tfor deleting all the files')   
+            else:
+                print('Unknown command: ', arguments[0])
+                print('Type delete_migrations_custom.py help for usage.')
+        else:
+            Clean()
+    except:
+        print('Unknown command: ', arguments[0])
+        print(sys.argv)
+        print('Type',fileName,'help for usage.')
+        
